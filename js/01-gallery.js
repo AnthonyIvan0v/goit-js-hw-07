b44b9d2c9ec2ad56cf843f8b3b90ce70c41beba8
AnthonyIvan0v <cat.sledgehammer@gmail.com>
2022-11-17 15:53:53 +0200

    создает галерею

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,40 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryRef = document.querySelector('.gallery');
+const cardsGallery = createItemGallery(galleryItems);
+
+galleryRef.innerHTML= cardsGallery;
+
+galleryRef.addEventListener('click', onCardsGallery);
+
+function createItemGallery(element) {
+    return element.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+                    </a>
+                </div>`}).join('');
+};
+
+
+function onCardsGallery(event) {
+    event.preventDefault();
+
+    if (event.target.classList.contains("gallery")) {
+        return
+    };
+    const source = event.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${source}"width="800" height="600">`);
+  instance.show();
+
+  window.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        instance.close();
+    };
+  })
+
+}
